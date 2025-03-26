65176bb54e1a427a048bde47dc2671dd995b8f27
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2025-03-26 14:39:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,24 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-
       home: Scaffold(
         appBar: AppBar(title: Text("Todo")),
         body: Center(
-          child: Column(children: [Text("data"), Text("data"), Text("data")]),
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Column(
+                children: [
+                  Text("Title"),
+                  Text("dec"),
+                ],
+              ),
+            )
+          ]),
         ),
       ),
     );
