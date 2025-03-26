53cf38c1fb68a31ea706d7d53aba1bd027850809
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2025-03-26 15:10:50 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,54 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(137, 39, 39, 39),
+        backgroundColor: const Color.fromARGB(136, 189, 189, 189),
         body: SafeArea(
           child: Center(
-            child: Column(children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.grey[300]),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Title"),
-                        Text("dec"),
-                      ],
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.done),
-                    )
-                  ],
-                ),
-              )
-            ]),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+                card(),
+              ]),
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Container card() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(color: Colors.grey[300]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Title"),
+              Text("dec"),
+            ],
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.done),
+          )
+        ],
       ),
     );
   }
