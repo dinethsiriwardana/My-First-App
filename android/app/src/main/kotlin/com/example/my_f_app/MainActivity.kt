493488ce0cee035449871370026390f121a44f12
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2025-03-26 14:11:00 +0530

    f commit

---
+++ android/app/src/main/kotlin/com/example/my_f_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_f_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(appBar: AppBar(title: Text('Hello World'))),
+    );
+  }
+}
